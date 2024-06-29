e97b96e77cd30bf4ffd8aedfff8d9ce0e2eee773
Axlolotl <axel_gaitan97@hotmail.com>
2024-06-29 19:16:33 +0200

    s

--- data_processing.py
+++ data_processing.py
@@ -57,7 +57,7 @@ flight_data.head()
 n_row = len(flight_data[:,0])
 ## Weather conditions
 flight_data['Weather'] = np.random.choice(['snow','rain','sun',
-                                           'fog','storm'], n_row)
+                                           'fog','storm'], 100)
 
 
 ## Seasonal factors

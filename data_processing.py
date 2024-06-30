00764c9444d081291632ce19eccadfc765e70a08
Kytusla908 <jordiren99@gmail.com>
2024-06-30 04:11:26 +0200

    Added the artificial variables

--- data_processing.py
+++ data_processing.py
@@ -5,7 +5,7 @@ import seaborn as sns
 
 
 
-flight_data = pd.read_csv('C:/Users/rapid/Desktop/HackBCN/Airlines.csv')
+flight_data = pd.read_csv('./Airlines.csv')
 flight_data.info()
 
 
@@ -49,29 +49,53 @@ flight_data['Route'] = ''
 for i in range(len(flight_data['Airline'])):
     flight_data['Route'][i] = flight_data['AirportFrom'][i]+'-'+flight_data['AirportTo'][i]
 
-# Check
+# Check =======================================
 flight_data.info()
-flight_data.head()
+flight_data.head(20)
+
+
 
 # Create new variables
-np.random.seed(42)
-n_row = len(flight_data[:,0])
+np.random.seed(42)  #Set random seed
+n_row = len(flight_data.iloc[:,0])
+
 ## Weather conditions
-flight_data['Weather'] = np.random.choice(['snow','rain','sun',
-                                           'fog','storm'], n_row)
+flight_data['Weather'] = 0
+np.random.seed(42)  #Set random seed
+### Different prob if there's delay or not
+for i in range(n_row):
+    if flight_data['Delay'][i] == 0:
+        weather = np.random.choice(['sun','rain','snow','fog','storm'], 
+                                                  1, p=[0.75,0.1,0.05,.05,.05])  # if there's NO delay, lower p for bad conditions 
+        flight_data['Weather'][i] = str(weather[0])  # to avoid type error: np.ndarray, we convert the cell data to string
+    elif flight_data['Delay'][i] == 1:
+        weather = np.random.choice(['sun','rain','snow','fog','storm'], 
+                                                  1, p=[0.2,0.5,0.1,.1,.1]) # if there's delay,  higher p for bad conditions
+        flight_data['Weather'][i] = str(weather[0])  # to avoid type error: np.ndarray, we convert the cell data to string
 
 
 ## Seasonal factors
-flight_data['Season'] = np.random.choice(['Winter', 'Spring', 'Summer', 'Autumn'], n_row)
+flight_data['Season'] = ''
+np.random.seed(42)  #Set random seed
+for i in range(n_row):
+    # Set the season to winter if it snowed
+    if flight_data['Weather'][i] == 'snow':
+        flight_data['Season'][i] = str('Winter')
+    # Else, random select season
+    else:
+        season = np.random.choice(['Spring', 'Summer', 'Autumn'], 1)
+        flight_data['Season'][i] = str(season[0])
 
 
 ## Holidays
-flight_data['Holiday'] = np.random.choice([0, 1], n_row, p=[0.9, 0.1]) # 10% of holiday probability, 1 holiday every 10 flight days
+flight_data['Holiday'] = np.random.choice([0, 1], n_row, p=[0.9, 0.1]) # 10% of holiday probability,
+                                                                       # 1 holiday every 10 flight days
+flight_data['Holiday'].value_counts()
 
 
 ## Political factors
 sanction_levels = ['No sanctions', 'Economic sanctions', 'Flight restrictions']
-flight_data['Sanction_Level'] = np.random.choice(sancion_levels, n_row, p=[0.7, 0.2, 0.1]) # 70%, 20%, 10%
+flight_data['Sanction_Level'] = np.random.choice(sanction_levels, n_row, p=[0.7, 0.2, 0.1]) # 70%, 20%, 10%
 
 flight_data['Security_Alert'] = np.random.choice([0, 1], n_row, p=[0.95, 0.05])
 
@@ -82,18 +106,39 @@ flight_data['Runway_Condition'] = np.random.choice(['Good', 'Fair', 'Poor'], n_r
 flight_data['Operational_Issues'] = np.random.choice([0, 1], n_row, p=[0.95, 0.05])
 
 
-airlines = flight_data['Airline'].unique()
+
+# Check =======================================
+flight_data.info()
+flight_data.head()
 
 
-flight_dummy = pd.get_dummies(flight_data,
-                              columns=['Airline','Flight',])
+# Create dummy variables
+## Get the non-numerical columns
+columns = flight_data.columns
+to_dummies = []
+for column in columns:
+    if flight_data[column].dtype == 'O':
+        to_dummies.append(column)
+
+## Create the dummy variables
+for column in to_dummies:
+    print(f'Converting {column} column')
+    flight_data = pd.get_dummies(flight_data, columns=[column], dtype=int)
 
 
-flight_data['AirportFrom'][0]+'-'+flight_data['AirportTo'][0]
+# Create the Response variable
+delay = pd.DataFrame(flight_data['Delay'])
+flight_data.drop(columns=['Delay'], inplace=True)
 
 
+# Final notes ==========================================================================
+#  When doing the train_test_split, the column flight_data['Delay'] will be the y.
+#  If necessary, extract the column with: delay = pd.DataFrame(flight_data['Delay'])
+#  and remove it from the flight_data dataframe.
 
-
+#  About training the models: if you have enough time, try to train them with the dummy
+#  variables dataset but also with the original dataset (without dummy conversion)
+# ======================================================================================
 
 
 
